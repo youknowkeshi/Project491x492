7c22984c842c598b689944ff4645196e3c49a49e
NithikonIndy <nithikon4410@hotmail.com>
2024-04-24 13:28:12 +0700

    .

---
+++ my-app/src/api/signIn.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosResponse } from 'axios';
+
+async function fetchData() {
+    try {
+        const response: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchData();
