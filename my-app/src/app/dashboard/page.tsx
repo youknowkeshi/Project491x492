9e409a8b9249d8538d87113a7ae3a52749d5f0d2
youknowkeshi <pongtanate12@gmail.com>
2024-04-30 16:24:08 +0700

    create dashboard and  register page

---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from 'react'
+import { Carousel } from "flowbite-react";
+
+type Props = {}
+
+export default function DashBoard({}: Props) {
+  return (
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <Carousel>
+      <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+      <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+      <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+      <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+      <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+    </Carousel>
+    <div className="grid h-56 grid-cols-2 gap-7 sm:h-64 xl:h-80 2xl:h-96 mt-7">
+      <Carousel>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+      <Carousel indicators={false}>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+  </div>
+  
+  )
+}
