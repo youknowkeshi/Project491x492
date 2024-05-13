8470a0efb951992186c2f3cdc0f650d2abaa4895
patthaveekarn <patthaveekarn2544@gmail.com>
2024-05-13 15:05:18 +0700

    aum

--- my-app/src/app/room/page.tsx
+++ my-app/src/app/room/page.tsx
@@ -8,27 +8,27 @@ export default function MyComponent() {
       <div className="flex items-start justify-center"> {/* Full screen height */}
       <div className="flex justify-center my-20">
         <Link href="/graph">
-        <div className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-6"> {/* First card */}
+        <div style={{ width: '400px', height: '150px' }} className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-6"> {/* First card */}
           
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
               Counselling Room 1
             </h5>
             <Divider className="dark:divide-gray-900">
           </Divider>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
+              (พี่ป๊อป)
             </p>
         </div>
         </Link>
         <Link href="/graph">
-        <div  className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-gray-900 dark:border-gray-700 dark:hover:bg-gray-700 mx-6 "> {/* Second card */}
+        <div style={{ width: '400px', height: '150px' }} className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-100 dark:bg-gray-900 dark:border-gray-700 dark:hover:bg-gray-700 mx-6 "> {/* Second card */}
           <h5  className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
             Counselling Room 2
           </h5>
           <Divider className="dark:divide-gray-900">
-          </Divider>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+</Divider>
+          <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
+            (นักจิตอาสา)
           </p>
         </div>
         </Link>
@@ -39,8 +39,10 @@ export default function MyComponent() {
     <div className="flex justify-center items-center "> {/* องค์ประกอบแรก */}
       {/* <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow space-y-10"> */}
         <div className="flex flex-col justify-between p-4">
-          <h5 className="mb-2 text-2xl font-bold">Title 1</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h5 className="mb-2 text-2xl font-bold">การเลือกห้อง</h5>
+          <p>1. คลิกเลือกห้องที่ต้องการ</p>
+          <p>1. คลิกเลือกห้องที่ต้องการ</p>
+          <p>1. คลิกเลือกห้องที่ต้องการ</p>
         </div>
       {/* </a> */}
     </div>
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,15 @@
+const plugin = require("flowbite/plugin");
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{html,js}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [plugin,flowbite.plugin(),],
+};
