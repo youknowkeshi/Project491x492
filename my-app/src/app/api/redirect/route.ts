911c6d63b7a51499a01f7e6be22b4dd6aee7097a
NithikonIndy <nithikon4410@hotmail.com>
2024-05-23 20:38:58 +0700

    tokens api

--- my-app/src/app/api/redirect/route.ts
+++ my-app/src/app/api/redirect/route.ts
@@ -1,60 +1,83 @@
 import { google } from "googleapis";
-import  { NextRequest, NextResponse } from 'next/server';
-import {oauth2Client} from "../logingoogle/route"
+import { NextRequest, NextResponse } from 'next/server';
+import { oauth2Client } from "../logingoogle/route";
+import { pool } from "../../lib/db";
 
-
-export async function POST(request:NextRequest ) {
-    try{
-        const req = await request.json();
-        const code = req.code
-        console.log("this is my code : ",code);
-        
-        
-        oauth2Client.getToken(code, (err, tokens) => {
-            if (err) {
-              // Handle error if token exchange fails
-              console.error('Couldn\'t get token', err);
-              return new NextResponse('Error');
-            }
-            
-            if (tokens) {
-                oauth2Client.setCredentials(tokens);
-            } else {
-                console.error('Tokens are null or undefined');
-                return new NextResponse('Error');
-            }
-        }); 
-       
-        return new NextResponse('Successfully logged in');
-
-    }catch(err){
-        console.log("This is : ",err);
-        return new NextResponse('Failed');
-    }
+// Function to convert timestamp to Thai time
+function convertTimestampToThaiTime(timestamp_ms: any): string {
+  const date = new Date(timestamp_ms);
+  const thaiTime = new Date(date.getTime() + 7 * 60 * 60 * 1000);
+  const formattedThaiTime = thaiTime.toISOString().replace('T', ' ').substring(0, 19);
+  return formattedThaiTime;
 }
 
-export async function GET(request:NextRequest) {
 
-    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-    // List all calendars
-    try {
-      // List all calendars
-      const response = await calendar.calendarList.list({});
-      
-      if (!response || !response.data || !response.data.items) {
-        // Handle case where response or necessary properties are undefined
-        console.error('No calendars found in the response', response);
-        return new NextResponse('No calendars found!');
+//not finish
+export async function POST(request: NextRequest) {
+  try {
+    const req = await request.json();
+    const code = req.code;
+
+    oauth2Client.getToken(code, async (err, tokens) => {
+      if (err) {
+        console.error('Couldn\'t get token', err);
+        return new NextResponse('Error');
       }
-  
-      // Send the list of calendars as JSON
-      const calendars = response.data.items;
-      return NextResponse.json(calendars);
-  
-    } catch (err) {
-      // Handle error if the API request fails
-      console.error('Error fetching calendars', err);
-      return new NextResponse('Error!');
-    }
-  }
 
+      oauth2Client.on('tokens', (tokens) => {
+        if (tokens.refresh_token) {
+          console.log(tokens.refresh_token);
+        }
+        console.log(tokens.access_token);
+      });
+
+      if (tokens) {
+        const { access_token, refresh_token, scope, token_type, expiry_date } = tokens;
+        const text = 'INSERT INTO admins(personid,firstname_lastname, cmuaccount, studentid, organization_name, accounttype, role) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+        const expiry = convertTimestampToThaiTime(expiry_date);
+        const values = [access_token, refresh_token, scope, token_type, expiry];
+
+        const client = await pool.connect();
+        try {
+          const res = await client.query(text, values);
+
+          if (res.rowCount === 0) {
+            return new NextResponse('User not found', { status: 404 });
+          }
+
+          return NextResponse.json({ res });
+        } finally {
+          client.release();
+        }
+      } else {
+        console.error('Tokens are null or undefined');
+        return new NextResponse('Error');
+      }
+    });
+
+    return NextResponse.json(oauth2Client);
+
+  } catch (err) {
+    console.error("Error:", err);
+    return new NextResponse('Failed');
+  }
+}
+
+export async function GET(request: NextRequest) {
+  const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
+  try {
+    const response = await calendar.calendarList.list({});
+    if (!response || !response.data || !response.data.items) {
+      console.error('No calendars found in the response', response);
+      return new NextResponse('No calendars found!');
+    }
+
+    const calendars = response.data.items;
+    return NextResponse.json(calendars);
+
+  } catch (err) {
+    console.error('Error fetching calendars', err);
+    return new NextResponse('Error!');
+  }
+}
