1e8bb39de0ad8e000764d468f983b15ae87f04da
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2024-05-30 13:25:07 +0700

    add responsive in bookinglist

--- my-app/src/app/profile/BookingList.tsx
+++ my-app/src/app/profile/BookingList.tsx
@@ -1,32 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Card, Button } from "antd";
 
-function bookinglist() {
+function BookingList() {
   return (
-    <div className='flex gap-4 items-center mt-5 border rounded-lg p-5'>
-      <img
-      src = "https://www.wellingtonregional.com/sites/wellingtonregional.com/files/doctors_visit_1200x900.jpg"
-      width= "200"
-      height= "200"
-      className='border rounded-lg ml-4'
-      />
-    
-      <div className='flex flex-col gap-4 p-5'>
-      <h2 className='font-bold text-[18px]'>
-          Dr : Pongtante Namsawat
-        </h2>
-        <h2>
-          Room 1,Faculty of Engineering
-        </h2>
-        <h2>
-          Time : 9.00-10.00 
-        </h2>
-        <h2>
-          Appointment : 16-Feb-2024
-        </h2>
-        
+    <Card className="mt-7 relative">
+      <div className="flex flex-col md:flex-row items-center">
+        <img
+          src="https://www.wellingtonregional.com/sites/wellingtonregional.com/files/doctors_visit_1200x900.jpg"
+          alt="Doctor Visit"
+          className="w-1/2 md:w-1/4 lg:w-1/6 border rounded-lg md:ml-4"
+        />
+
+        <div className="flex flex-col gap-4 p-5 text-center md:text-left">
+          <h2 className="font-bold text-[18px]">Dr : Pongtante Namsawat</h2>
+          <h2>Room 1, Faculty of Engineering</h2>
+          <h2>Time : 9.00-10.00</h2>
+          <h2>Appointment : 16-Feb-2024</h2>
+        </div>
       </div>
-    </div>
-  )
+
+      <Button type="primary" danger className="absolute bottom-4 right-4">
+        Cancel
+      </Button>
+    </Card>
+  );
 }
 
-export default bookinglist
+export default BookingList;
