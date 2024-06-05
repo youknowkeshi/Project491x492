6c3711e1af94a04bfe3bebc8ba32612729c0dd76
patthaveekarn <patthaveekarn2544@gmail.com>
2024-06-05 15:50:43 +0700

    code

--- my-app/src/app/code/page.tsx
+++ my-app/src/app/code/page.tsx
@@ -9,6 +9,7 @@ export default function MePage() {
 
    const generateNewString = () => {
        setGeneratedString(uniqueString());
+       setCopySuccess("");
    };
 
    const copyToClipboard = async () => {
@@ -24,22 +25,26 @@ export default function MePage() {
        <>
            <Nav />
            <div className="mt-10">
-               <div className="flex flex-col items-start justify-center">
-                   <h2 className="text-2xl mb-4">Random Access Code Generator</h2>
-                   <p className="text-lg mb-4">Generated Unique String: {generatedString}</p>
-                   <button
+
+            <h2 className="text-2xl mb-4">Random Access Code Generator</h2>
+               <div className="flex flex-row items-start justify-center">
+                   
+                   <div>
+                        <p className="text-lg mb-4">Generated Unique String: {generatedString}</p>
+                        <button
                        onClick={generateNewString}
                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4"
-                   >
+                        >
                        Generate New Unique String
                    </button>
+                   </div>
                    <button
-                       onClick={copyToClipboard}
-                       className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
-                   >
-                       Copy to Clipboard
-                   </button>
-                   {copySuccess && <p className="mt-2 text-green-500">{copySuccess}</p>}
+                        onClick={copyToClipboard}
+                        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 ml-5 mr-3"
+                        >
+                        Copy to Clipboard
+                    </button>
+                    {copySuccess && <p className="mt-2 text-green-500">{copySuccess}</p>}
                </div>
            </div>
        </>
