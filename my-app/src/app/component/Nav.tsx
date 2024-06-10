759c9b7c785122abdbaae5fed3ba28e68d16dac4
NithikonIndy <nithikon4410@hotmail.com>
2024-06-10 14:17:25 +0700

    .

--- my-app/src/app/component/Nav.tsx
+++ my-app/src/app/component/Nav.tsx
@@ -3,14 +3,14 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Footer, Navbar } from "flowbite-react";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import { getCookie } from "cookies-next";
-
-
 
 export function Nav() {
+  const admin = process.env.NEXT_PUBLIC_ADMIN as string;
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
   const router = useRouter();
 
   function signOut() {
@@ -19,18 +19,21 @@ export function Nav() {
     });
   }
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/api/register');
-        const token = response.data.ok
+        const response = await axios.get("/api/register");
+        const token = response.data.ok;
+        const adminCMUAccount = response.data.cmuAccount;
+
         if (token) {
           setIsLoggedIn(true);
         }
+        if (adminCMUAccount === admin) {
+          setIsAdmin(true);
+        }
       } catch (err) {
         console.log("This is error: ", err);
-
       }
     };
 
@@ -50,31 +53,47 @@ export function Nav() {
         </div>
         <Navbar.Toggle />
         <Navbar fluid rounded>
-        <Navbar.Collapse >
+          <Navbar.Collapse>
             {isLoggedIn ? (
-              <>
-                <Navbar.Link href="/dashboard" active>
-                  Home
-                </Navbar.Link>
-                <Navbar.Link as={Link} href="/register">
-                  Register
-                </Navbar.Link>
-                <Navbar.Link href="/appointment">Appointment</Navbar.Link>
-                <Navbar.Link href="/profile">Profile</Navbar.Link>
-                <Navbar.Link href="/Evaluation">Evaluation</Navbar.Link>
-                <Navbar.Link href="/article">Article</Navbar.Link>
-                <Navbar.Link onClick={signOut}>Logout</Navbar.Link>
-
-
-              </>
+              isAdmin ? (
+                <>
+                  <Navbar.Link href="/dashboard" active>
+                    Home
+                  </Navbar.Link>
+                  <Navbar.Link as={Link} href="/register">
+                    Register
+                  </Navbar.Link>
+                  <Navbar.Link href="/appointment">Appointment</Navbar.Link>
+                  <Navbar.Link href="/information">Information</Navbar.Link>
+                  <Navbar.Link href="/report">Report</Navbar.Link>
+                  <Navbar.Link href="/make">Make Appointment</Navbar.Link>
+                  <Navbar.Link href="/article">Article</Navbar.Link>
+                  <Navbar.Link href="/evaluation-form">Evaluation Form</Navbar.Link>
+                  <Navbar.Link href="/graph">Graph</Navbar.Link>
+                  <Navbar.Link onClick={signOut}>Logout</Navbar.Link>
+                </>
+              ) : (
+                <>
+                  <Navbar.Link href="/dashboard" active>
+                    Home
+                  </Navbar.Link>
+                  <Navbar.Link as={Link} href="/register">
+                    Register
+                  </Navbar.Link>
+                  <Navbar.Link href="/appointment">Appointment</Navbar.Link>
+                  <Navbar.Link href="/profile">Profile</Navbar.Link>
+                  <Navbar.Link href="/evaluation">Evaluation</Navbar.Link>
+                  <Navbar.Link href="/article">Article</Navbar.Link>
+                  <Navbar.Link onClick={signOut}>Logout</Navbar.Link>
+                </>
+              )
             ) : (
               <Navbar.Link href={process.env.NEXT_PUBLIC_CMU_OAUTH_URL}>Login</Navbar.Link>
             )}
           </Navbar.Collapse>
         </Navbar>
-
       </Navbar>
-      <hr className="mt-4"></hr>
+      <hr className="mt-4" />
     </>
   );
 }
