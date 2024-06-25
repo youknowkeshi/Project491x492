d3f47d6b54fe2e41fe42666b880360f2555c5bba
youknowkeshi <pongtanate12@gmail.com>
2024-06-25 14:37:55 +0700

    add modal

--- my-app/src/app/List/page.tsx
+++ my-app/src/app/List/page.tsx
@@ -17,10 +17,10 @@ export default function page({}: Props) {
       <h1 className="mt-7 text-xl tracking-tight text-gray-900 dark:text-white ml-1">
         Appointment List
       </h1>
-      <Card className="border-r-4 border-l-4 border-x-cyan-300 mt-3 mb-4 p-4 relative  ">
+      <Card className="border-r-4 border-l-4 border-x-cyan-300 mt-7 mb-4 p-4 relative  ">
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Time : 9.00 - 10.00
-        </h5>
+        </h5> 
         <hr></hr>
         <p className="font-normal text-gray-700 dark:text-gray-400">
           Name : Pongtanate Namsawat
