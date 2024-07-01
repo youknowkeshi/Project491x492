7e66d3737b02a86089d1e47d56475eac3d890731
patthaveekarn <patthaveekarn2544@gmail.com>
2024-07-01 14:16:35 +0700

    update

--- my-app/src/app/genkey/page.tsx
+++ my-app/src/app/genkey/page.tsx
@@ -35,10 +35,10 @@ export default function MePage() {
                 <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
                 </Carousel>
             </div>
-                <h2 className="text-2xl mb-8">Random Access Code Generator</h2>
+                <h2 className="text-2xl mb-7 mt-7">Random Access Code Generator</h2>
                 <button
                     onClick={generateNewString}
-                    className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-8"
+                    className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-7"
                 >
                     Generate 
                 </button>
