cf8c3de614d138152a2b64464a3b9c75d2f46797
youknowkeshi <pongtanate12@gmail.com>
2024-07-16 14:18:45 +0700

    loading

--- my-app/src/app/loading.tsx
+++ my-app/src/app/loading.tsx
@@ -5,13 +5,38 @@ type Props = {};
 export default function loading({}: Props) {
   return (
     <div>
-      <div className="relative flex justify-center items-centerrr">
-        <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
-        <img
-          src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"
-          className="rounded-full h-28 w-28 "
-        ></img>
-      </div>
+      <main className="bg-white">
+        <div className="mx-auto max-w-7xl px-4  sm:px-6 lg:px-8">
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+              aria-hidden="true"
+            >
+              {/* <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] clip-custom"></div> */}
+            </div>
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  <div className="relative flex justify-center items-center">
+                    <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+                    <img
+                      src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"
+                      className="rounded-full h-28 w-28"
+                    ></img>
+                  </div>
+                </div>
+              </div>
+              <div className="text-center"></div>
+            </div>
+            <div
+              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+              aria-hidden="true"
+            >
+              {/* <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] clip-custom"></div> */}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
