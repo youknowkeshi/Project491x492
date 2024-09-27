a36d403582039a272196b0b923ada6d64b60ef8b
NithikonIndy <nithikon4410@hotmail.com>
2024-09-27 22:26:12 +0700

    path

--- my-app/src/app/component/Navbaradmin.tsx
+++ my-app/src/app/component/Navbaradmin.tsx
@@ -90,7 +90,6 @@ export function Navbaradmin() {
               <div className="ml-4 flex items-center md:ml-6">
                 <div className="relative ml-3">
                   <a
-                    href="/"
                     className="rounded-md px-3 py-2 text-md font-semibold text-zinc-100 hover:bg-[#8FC1E3] hover:text-zinc-100"
                     onClick={signOut}
                   >
