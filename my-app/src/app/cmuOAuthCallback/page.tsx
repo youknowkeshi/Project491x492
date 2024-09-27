49b8f5adb3fe8b148283af71e32190f11b2b7c51
NithikonIndy <nithikon4410@hotmail.com>
2024-09-27 21:34:43 +0700

    setCookie

--- my-app/src/app/cmuOAuthCallback/page.tsx
+++ my-app/src/app/cmuOAuthCallback/page.tsx
@@ -37,48 +37,12 @@ export default function CMUOAuthCallback() {
     if (!code) return;
 
     axios
-      .post<SignInResponse>("/api/signIn", { authorizationCode: code })
-      .then((resp) => {
-        console.log("cmu", resp.data);
-        if (resp.data.ok) {
-
-          console.log("if",resp.data);
-
-          try {
-            //   const token = jwt.sign(
-            //     {
-            //       cmuAccount: resp.data.cmuBasicInfo.cmuitaccount,
-            //       firstName: resp.data.cmuBasicInfo.firstname_EN,
-            //       lastName: resp.data.cmuBasicInfo.lastname_EN,
-            //       studentId: resp.data.cmuBasicInfo.student_id, //Note that not everyone has this. Teachers and CMU Staffs don't have student id!
-            //       organization_name_EN: resp.data.cmuBasicInfo.organization_name_EN,
-            //       itaccounttype_EN: resp.data.cmuBasicInfo.itaccounttype_EN,
-            //     },
-            //     process.env.JWT_SECRET!,
-            //     {
-            //       expiresIn: "24h",
-            //     }
-            //   );
-
-            // // //Write token in cookie storage of client's browser
-            // // //Note that this is server side code. We can write client cookie from the server. This is normal.
-            // // //You can view cookie in the browser devtools (F12). Open tab "Application" -> "Cookies"
-            //   setCookie("cmu-oauth-example-token", token, {
-            //     maxAge: 3600 * 24,
-            //     secure: true,
-            //     sameSite: 'none',
-
-            //   });
-
-
-          } catch (error) {
-            console.error('JWT Signing Error:', error);
-          }
-
-          //getUsers();
-
-        }
-      })
+    .post<SignInResponse>("/api/signIn", { authorizationCode: code })
+    .then((resp) => {
+      if (resp.data.ok) {
+        getUsers();
+      }
+    })
       .catch((error: AxiosError<SignInResponse>) => {
         if (!error.response) {
           setMessage(
--- my-app/src/pages/api/signIn.ts
+++ my-app/src/pages/api/signIn.ts
@@ -21,7 +21,7 @@ async function getOAuthAccessTokenAsync(
   authorizationCode: string
 ): Promise<string | null> {
   try {
-
+    
     const response = await axios.post(
       process.env.CMU_OAUTH_GET_TOKEN_URL as string,
       {},
@@ -51,8 +51,8 @@ async function getCMUBasicInfoAsync(accessToken: string) {
       {
         headers: { Authorization: "Bearer " + accessToken },
       }
-
-
+      
+      
     );
     return response.data as CmuOAuthBasicInfo;
   } catch (err) {
@@ -89,11 +89,47 @@ export default async function handler(
     return res
       .status(400)
       .json({ ok: false, message: "Cannot get cmu basic info" });
-      
 
-  return res.json({ ok: true, ...cmuBasicInfo });
+
+
+  if (typeof process.env.JWT_SECRET !== "string")
+    throw "Please assign jwt secret in .env!";
+
+  const token = jwt.sign(
+    {
+      cmuAccount: cmuBasicInfo.cmuitaccount,
+      firstName: cmuBasicInfo.firstname_EN,
+      lastName: cmuBasicInfo.lastname_EN,
+      studentId: cmuBasicInfo.student_id, //Note that not everyone has this. Teachers and CMU Staffs don't have student id!
+      organization_name_EN: cmuBasicInfo.organization_name_EN,
+      itaccounttype_EN: cmuBasicInfo.itaccounttype_EN,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h", // Token will last for one hour only
+    }
+  );
+
+  //Write token in cookie storage of client's browser
+  //Note that this is server side code. We can write client cookie from the server. This is normal.
+  //You can view cookie in the browser devtools (F12). Open tab "Application" -> "Cookies"
+  setCookie("cmu-oauth-example-token", token, {
+    req,
+    res,
+    maxAge: 3600*24,
+    //Set httpOnly to true so that client JavaScript cannot read or modify token
+    //And the created token can be read by server side only
+    // httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+
+    // path: "/",
+    //change to your hostname in production
+    // domain: "localhost",
+  });
+
+  return res.json({ ok: true });
 }
-
 
 
 
