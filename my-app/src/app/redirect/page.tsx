2f2f9a6f2f9afa719a6f91e17a5bda90615ba76b
NithikonIndy <nithikon4410@hotmail.com>
2024-10-07 22:02:04 +0700

    docker

--- my-app/src/app/redirect/page.tsx
+++ my-app/src/app/redirect/page.tsx
@@ -33,34 +33,33 @@ export default function Home() {
             const response = await axios.post(apiUrl, { code });
 
             const Info = response.data.info;
-            console.log(Info);
-            
+         
             if (!Info) {
                 throw new Error('Info not found in API response');
             }          
 
             try {
 
-                // const token = jwt.sign(
-                //     {
-                //         id: Info.id,
-                //         email: Info.email,
-                //         verified_email: Info.verified_email,
-                //         name: Info.name
-                //     },
-                //     process.env.NEXT_PUBLIC_JWT_SECRET_GOOGLE!,
-                //     {
-                //         expiresIn: "24h",
-                //     }
-                // );
+                const token = jwt.sign(
+                    {
+                        id: Info.id,
+                        email: Info.email,
+                        verified_email: Info.verified_email,
+                        name: Info.name
+                    },
+                    process.env.NEXT_PUBLIC_JWT_SECRET_GOOGLE!,
+                    {
+                        expiresIn: "24h",
+                    }
+                );
 
-                // setCookie("google-oauth-example-token", token, {
-                //     maxAge: 3600 * 24,
-                //     //httpOnly: true, // ตั้งเป็น true ในการผลิต
-                //     secure: true,
-                //     sameSite: 'none',
-                //     // path: "/",
-                // });
+                setCookie("google-oauth-example-token", token, {
+                    maxAge: 3600 * 24,
+                    //httpOnly: true, // ตั้งเป็น true ในการผลิต
+                    secure: true,
+                    sameSite: 'none',
+                    // path: "/",
+                });
                 
                 // setCookie("test-token", token)
 
